ec71b554dcd544eb68eaa4240520407244f2b3dc
shrouk406 <shroukgomaa13@gmail.com>
2022-12-14 13:51:19 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+void main(void)
+{
+	char letter;
+	for (letter = 'a', letter <= 'z', letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
