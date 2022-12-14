3c94c619b573397aeacbea31f178d1a87cffc514
shrouk406 <shroukgomaa13@gmail.com>
2022-12-14 11:53:01 -0800

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,13 +3,16 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-int main(void)
+int main (void)
 {
-char *str;
-int x;
-str = "_putchar";
-for (x = 0; x < 8; x++)
-_putchar(str[x]);
-_putchar('\n');
-return (0);
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar('\n');
+	return(0);
 }
