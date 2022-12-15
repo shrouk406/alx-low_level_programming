3a2515f6c6a112a16b4cacc3651417e720aa8f05
shrouk406 <shroukgomaa13@gmail.com>
2022-12-15 12:15:49 -0800

    advanced

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -12,4 +12,5 @@ extern void print_diagonal(int n);
 extern void print_square(int size);
 extern void print_triangle(int size);
 extern void print_number(int n);
+extern void print_number(int n);
 #endif
